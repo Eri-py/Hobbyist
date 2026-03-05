98b0c963fdf7c46ea29c2aed9c6070516db497e4
Erioluwa Olajorin <eaolajorin06@gmail.com>
2026-03-04 22:16:51 -0600

    removed debug strings

--- WebServer/Hobbyist.Api/Program.cs
+++ WebServer/Hobbyist.Api/Program.cs
@@ -39,10 +39,6 @@ builder.Services.AddCors(options =>
     );
 });
 
-Console.WriteLine($"Jwt:Secret is null: {builder.Configuration["Jwt:Secret"] == null}");
-Console.WriteLine($"Jwt:Issuer is null: {builder.Configuration["Jwt:Issuer"] == null}");
-Console.WriteLine($"Jwt:Audience is null: {builder.Configuration["Jwt:Audience"] == null}");
-
 builder.Services.AddDatabases(builder.Configuration);
 builder.Services.AddAuthServices(builder.Configuration);
 builder.Services.AddScoped<IOtpService, OtpService>();
