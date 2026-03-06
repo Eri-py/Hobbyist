b2023e1e1e66f43bb8e7ca3a87c8d8fdc009aa2b
Erioluwa Olajorin <eaolajorin06@gmail.com>
2026-03-06 00:40:40 -0600

    - Now throws errors if secret, issuer, and audience are missing in jwt registration.

--- WebServer/Hobbyist.Api/Extensions/ServiceRegistrations/JwtAuthenticationRegistration.cs
+++ WebServer/Hobbyist.Api/Extensions/ServiceRegistrations/JwtAuthenticationRegistration.cs
@@ -4,13 +4,23 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Hobbyist.Api.Extensions.ServiceRegistrations;
 
-public static class AuthServicesRegistration
+public static class JwtAuthenticationRegistration
 {
     public static void AddJwtAuthentication(
         this IServiceCollection services,
         IConfiguration configs
     )
     {
+        var secret =
+            configs["Jwt:Secret"]
+            ?? throw new InvalidOperationException("Missing 'Jwt:Secret' configuration.");
+        var issuer =
+            configs["Jwt:Issuer"]
+            ?? throw new InvalidOperationException("Missing 'Jwt:Issuer' configuration.");
+        var audience =
+            configs["Jwt:Audience"]
+            ?? throw new InvalidOperationException("Missing 'Jwt:Audience' configuration.");
+
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
@@ -18,14 +28,12 @@ public static class AuthServicesRegistration
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
-                    ValidIssuer = configs["Jwt:Issuer"],
+                    ValidIssuer = issuer,
                     ValidateAudience = true,
-                    ValidAudience = configs["Jwt:Audience"],
+                    ValidAudience = audience,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(configs["Jwt:Secret"]!)
-                    ),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
                 };
                 options.Events = new JwtBearerEvents
                 {
