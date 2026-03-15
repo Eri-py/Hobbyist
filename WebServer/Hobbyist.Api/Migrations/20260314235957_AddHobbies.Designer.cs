e42e4a4a54389729f1d6ffc614496cd246302366
Erioluwa Olajorin <eaolajorin06@gmail.com>
2026-03-14 19:28:08 -0500

    - Fixed migration script to now seed hobbies with name in lowercase.

--- WebServer/Hobbyist.Api/Migrations/20260314235957_AddHobbies.Designer.cs
+++ WebServer/Hobbyist.Api/Migrations/20260314235957_AddHobbies.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hobbyist.Api.Migrations
 {
     [DbContext(typeof(HobbyistDbContext))]
-    [Migration("20260314063818_add_hobbies")]
-    partial class add_hobbies
+    [Migration("20260314235957_AddHobbies")]
+    partial class AddHobbies
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -43,92 +43,92 @@ namespace Hobbyist.Api.Migrations
                         new
                         {
                             Id = new Guid("2f7a4f09-c6b7-42dd-a646-b6f36e25b0c1"),
-                            Name = "Trading Cards"
+                            Name = "trading cards"
                         },
                         new
                         {
                             Id = new Guid("8d9458bb-2303-442b-a4df-08f6bf6bf4f2"),
-                            Name = "Vinyl Records"
+                            Name = "vinyl records"
                         },
                         new
                         {
                             Id = new Guid("1d3b89e5-3fc0-4f6c-bded-53afe9946f0a"),
-                            Name = "Coins"
+                            Name = "coins"
                         },
                         new
                         {
                             Id = new Guid("f9c8f2a6-c5cf-42bb-9f72-701a8e39b539"),
-                            Name = "Stamps"
+                            Name = "stamps"
                         },
                         new
                         {
                             Id = new Guid("a90d8e15-dad2-4894-a9ca-b5e3f3f42c57"),
-                            Name = "Comics"
+                            Name = "comics"
                         },
                         new
                         {
                             Id = new Guid("6e662a47-fde0-4546-ac66-bf0ddfdf1a27"),
-                            Name = "Figures"
+                            Name = "figures"
                         },
                         new
                         {
                             Id = new Guid("ea76d9f2-08a7-4b38-83ef-e4f0d8b734ab"),
-                            Name = "Ceramics"
+                            Name = "ceramics"
                         },
                         new
                         {
                             Id = new Guid("5e4a94f2-a021-48ab-b2c0-a79a6d6ff43d"),
-                            Name = "Art Prints"
+                            Name = "art prints"
                         },
                         new
                         {
                             Id = new Guid("f56b64ac-f453-4f4f-8730-8d1b7e3fda08"),
-                            Name = "Vintage Toys"
+                            Name = "vintage toys"
                         },
                         new
                         {
                             Id = new Guid("6fccf7e8-1615-422f-a654-210779464fc8"),
-                            Name = "Watches"
+                            Name = "watches"
                         },
                         new
                         {
                             Id = new Guid("b197c6aa-2c48-4a03-b8a5-c190d900f8e5"),
-                            Name = "Jewellery"
+                            Name = "jewellery"
                         },
                         new
                         {
                             Id = new Guid("9b6f7d4e-687a-4f8a-85cc-682ec8905196"),
-                            Name = "Books"
+                            Name = "books"
                         },
                         new
                         {
                             Id = new Guid("f492c588-a045-4466-8f26-0fd9bb90989e"),
-                            Name = "Cameras"
+                            Name = "cameras"
                         },
                         new
                         {
                             Id = new Guid("6eadbe6c-20ba-4f9a-9196-6ae0ac88d7bb"),
-                            Name = "Video Games"
+                            Name = "video games"
                         },
                         new
                         {
                             Id = new Guid("859c18b6-ef67-4f31-a84d-686f0adf4512"),
-                            Name = "Rare Plants"
+                            Name = "rare plants"
                         },
                         new
                         {
                             Id = new Guid("5d22fd82-f357-45dc-be7b-1a22ff13d9d0"),
-                            Name = "Minerals"
+                            Name = "minerals"
                         },
                         new
                         {
                             Id = new Guid("19da3f2b-736f-436b-84de-7d930c3f56fb"),
-                            Name = "Sneakers"
+                            Name = "sneakers"
                         },
                         new
                         {
                             Id = new Guid("3fcd0f37-9401-43fb-b9ad-f5734b396497"),
-                            Name = "Antique Maps"
+                            Name = "antique maps"
                         });
                 });
 
--- WebServer/Hobbyist.Api/Migrations/20260314235957_AddHobbies.cs
+++ WebServer/Hobbyist.Api/Migrations/20260314235957_AddHobbies.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Hobbyist.Api.Migrations
 {
     /// <inheritdoc />
-    public partial class add_hobbies : Migration
+    public partial class AddHobbies : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -58,24 +58,24 @@ namespace Hobbyist.Api.Migrations
                 columns: new[] { "Id", "Name" },
                 values: new object[,]
                 {
-                    { new Guid("19da3f2b-736f-436b-84de-7d930c3f56fb"), "Sneakers" },
-                    { new Guid("1d3b89e5-3fc0-4f6c-bded-53afe9946f0a"), "Coins" },
-                    { new Guid("2f7a4f09-c6b7-42dd-a646-b6f36e25b0c1"), "Trading Cards" },
-                    { new Guid("3fcd0f37-9401-43fb-b9ad-f5734b396497"), "Antique Maps" },
-                    { new Guid("5d22fd82-f357-45dc-be7b-1a22ff13d9d0"), "Minerals" },
-                    { new Guid("5e4a94f2-a021-48ab-b2c0-a79a6d6ff43d"), "Art Prints" },
-                    { new Guid("6e662a47-fde0-4546-ac66-bf0ddfdf1a27"), "Figures" },
-                    { new Guid("6eadbe6c-20ba-4f9a-9196-6ae0ac88d7bb"), "Video Games" },
-                    { new Guid("6fccf7e8-1615-422f-a654-210779464fc8"), "Watches" },
-                    { new Guid("859c18b6-ef67-4f31-a84d-686f0adf4512"), "Rare Plants" },
-                    { new Guid("8d9458bb-2303-442b-a4df-08f6bf6bf4f2"), "Vinyl Records" },
-                    { new Guid("9b6f7d4e-687a-4f8a-85cc-682ec8905196"), "Books" },
-                    { new Guid("a90d8e15-dad2-4894-a9ca-b5e3f3f42c57"), "Comics" },
-                    { new Guid("b197c6aa-2c48-4a03-b8a5-c190d900f8e5"), "Jewellery" },
-                    { new Guid("ea76d9f2-08a7-4b38-83ef-e4f0d8b734ab"), "Ceramics" },
-                    { new Guid("f492c588-a045-4466-8f26-0fd9bb90989e"), "Cameras" },
-                    { new Guid("f56b64ac-f453-4f4f-8730-8d1b7e3fda08"), "Vintage Toys" },
-                    { new Guid("f9c8f2a6-c5cf-42bb-9f72-701a8e39b539"), "Stamps" },
+                    { new Guid("19da3f2b-736f-436b-84de-7d930c3f56fb"), "sneakers" },
+                    { new Guid("1d3b89e5-3fc0-4f6c-bded-53afe9946f0a"), "coins" },
+                    { new Guid("2f7a4f09-c6b7-42dd-a646-b6f36e25b0c1"), "trading cards" },
+                    { new Guid("3fcd0f37-9401-43fb-b9ad-f5734b396497"), "antique maps" },
+                    { new Guid("5d22fd82-f357-45dc-be7b-1a22ff13d9d0"), "minerals" },
+                    { new Guid("5e4a94f2-a021-48ab-b2c0-a79a6d6ff43d"), "art prints" },
+                    { new Guid("6e662a47-fde0-4546-ac66-bf0ddfdf1a27"), "figures" },
+                    { new Guid("6eadbe6c-20ba-4f9a-9196-6ae0ac88d7bb"), "video games" },
+                    { new Guid("6fccf7e8-1615-422f-a654-210779464fc8"), "watches" },
+                    { new Guid("859c18b6-ef67-4f31-a84d-686f0adf4512"), "rare plants" },
+                    { new Guid("8d9458bb-2303-442b-a4df-08f6bf6bf4f2"), "vinyl records" },
+                    { new Guid("9b6f7d4e-687a-4f8a-85cc-682ec8905196"), "books" },
+                    { new Guid("a90d8e15-dad2-4894-a9ca-b5e3f3f42c57"), "comics" },
+                    { new Guid("b197c6aa-2c48-4a03-b8a5-c190d900f8e5"), "jewellery" },
+                    { new Guid("ea76d9f2-08a7-4b38-83ef-e4f0d8b734ab"), "ceramics" },
+                    { new Guid("f492c588-a045-4466-8f26-0fd9bb90989e"), "cameras" },
+                    { new Guid("f56b64ac-f453-4f4f-8730-8d1b7e3fda08"), "vintage toys" },
+                    { new Guid("f9c8f2a6-c5cf-42bb-9f72-701a8e39b539"), "stamps" },
                 }
             );
 
