bbddda1005f8aaf98360ae0d374ea3854ad746ff
Erioluwa Olajorin <eaolajorin06@gmail.com>
2026-05-05 19:50:53 -0400

    - replace expo image picker with expo media library + expo camera for more control over user library rendering.

--- Mobile/src/app/(create)/index.tsx
+++ Mobile/src/app/(create)/index.tsx
@@ -1,11 +1,72 @@
-import { Text } from "react-native-paper";
+import * as MediaLibrary from "expo-media-library";
+import { StyleSheet, Image, Button, View, Text } from "react-native";
+import { useEffect, useState } from "react";
 
 import { ThemedView } from "@/components/shared/ThemedView";
 
 export default function Create() {
+  const [albums, setAlbums] = useState<MediaLibrary.Album[] | null>(null);
+
+  async function getAlbums() {
+    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
+      includeSmartAlbums: true,
+    });
+    setAlbums(fetchedAlbums);
+  }
+
   return (
-    <ThemedView>
-      <Text>Create post</Text>
-    </ThemedView>
+    <View style={styles.container}>
+      <Button onPress={getAlbums} title="Get albums" />
+      <ThemedView>
+        {albums && albums.map((album) => <AlbumEntry key={album.id} album={album} />)}
+      </ThemedView>
+    </View>
   );
 }
+
+type AlbumEntryTypes = {
+  album: MediaLibrary.Album;
+};
+
+function AlbumEntry({ album }: AlbumEntryTypes) {
+  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    async function getAlbumAssets() {
+      const albumAssets = await MediaLibrary.getAssetsAsync({ album });
+      setAssets(albumAssets.assets);
+    }
+    getAlbumAssets();
+  }, [album]);
+
+  return (
+    <View key={album.id} style={styles.albumContainer}>
+      <Text>
+        {album.title} - {album.assetCount ?? "no"} assets
+      </Text>
+      <View style={styles.albumAssetsContainer}>
+        {assets &&
+          assets.map((asset) => (
+            <Image key={asset.id} source={{ uri: asset.uri }} width={50} height={50} />
+          ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 8,
+    justifyContent: "center",
+  },
+  albumContainer: {
+    paddingHorizontal: 20,
+    marginBottom: 12,
+    gap: 4,
+  },
+  albumAssetsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
